65c9c823bef7b5b61eb26e4fb8c7652f72f904d7
Dale O Eason <doeason@comcast.net>
2024-05-18 20:12:49 -0500

    Update skysolveSetup.sh
    Modifying installing extra python modules

--- skysolveSetup.sh
+++ skysolveSetup.sh
@@ -402,6 +402,8 @@ fi
 
 
 #install python scipy
+echo "installing extra python modules"
+sudo apt --fix-broken install
 sudo apt-get install scipy
 sudo apt install python3-matplotlib
 echo "This next step may take 10 to 15 minutes."
